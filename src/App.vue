a98106bd37a1e619ba50f7bb6f03a5e4b45d9c47
fengqian <949637749@qq.com>
2021-07-05 21:21:56 +0800

    继承 ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="nav">
+    <a-button type="primary">Primary</a-button>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
-createApp(App).use(store).use(router).mount('#app')
+
+const app = createApp(App)
+app.use(Antd);
+app.use(store).use(router)
+app.mount('#app')
